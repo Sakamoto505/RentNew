08d57b8ac53459493a7b57a888f1bab7b9a6332f
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2025-06-18 17:57:45 +0400

    add all  origins '*'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
-    resource(
-      '*',
-      headers: :any,
-      expose: ['Authorization'],
-      methods: [:get, :patch, :put, :delete, :post, :options, :show]
-    )
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             expose: ['Authorization'],
+             methods: [:get, :post, :patch, :put, :delete, :options]
   end
 end
