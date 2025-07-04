94e23fac0b0bad912350f648fc8c9ca5f089c526
potatofreedom <gustovsson6@gmail.com>
2025-07-04 22:10:00 +0700

    added host cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://rentavtokavkaz.ru', 'http://localhost:5175'
 
     resource '*',
              headers: :any,
              expose: ['Authorization'],
-             methods: [:get, :post, :patch, :put, :delete, :options]
+             methods: [:get, :post, :patch, :put, :delete, :options],
+             credentials: true
   end
 end
