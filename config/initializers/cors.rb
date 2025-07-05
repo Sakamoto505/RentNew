da85c3f7d48620a80b1ee0050a1d711ccb4df7a0
potatofreedom <gustovsson6@gmail.com>
2025-07-05 14:50:07 +0700

    fixed cors for test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://rentavtokavkaz.ru', 'http://localhost:5175'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://rentavtokavkaz.ru', 'http://localhost:5175'
 
-    resource '*',
-             headers: :any,
-             expose: ['Authorization'],
-             methods: [:get, :post, :patch, :put, :delete, :options],
-             credentials: true
-  end
-end
+#     resource '*',
+#              headers: :any,
+#              expose: ['Authorization'],
+#              methods: [:get, :post, :patch, :put, :delete, :options],
+#              credentials: true
+#   end
+# end
