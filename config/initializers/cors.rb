cc1fc82cd418e0c2f3fecb2e525066aa51befa6a
potatofreedom <gustovsson6@gmail.com>
2025-07-06 20:36:09 +0700

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins lambda { |origin, _env| origin }
+    origins '*'
 
     resource '/api/*',
              headers: :any,
              expose: ['Authorization'],
              methods: [:get, :post, :patch, :put, :delete, :options],
-             credentials: true
+             credentials: false
   end
 end
