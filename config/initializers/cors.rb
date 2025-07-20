e9dd3219815d895e70b30277ca5fdec6b3413948
potatofreedom <gustovsson6@gmail.com>
2025-07-20 19:51:05 +0700

    added ip

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,22 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins do |origin, env|
-      origin
-    end
+    origins 'http://localhost:5175', 'http://77.87.99.172:5175', 'http://213.142.146.40:5175', 'https://rentavtokavkaz.ru'
 
     resource '/api/*',
              headers: :any,
              expose: ['Authorization'],
              methods: [:get, :post, :patch, :put, :delete, :options],
              credentials: true
+  end
+  
+  allow do
+    origins '*'
+    
+    resource '/api/*',
+             headers: :any,
+             expose: ['Authorization'],
+             methods: [:get, :post, :patch, :put, :delete, :options],
+             credentials: false
   end
 end
