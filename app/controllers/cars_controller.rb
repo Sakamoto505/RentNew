83155401002c4f23167565ca2a37007481f1257b
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2025-08-22 18:20:40 +0400

    bulk_show fix

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :bulk_show]
 
   def index
     cars = Car.includes(:car_images, :user).order(created_at: :desc)
