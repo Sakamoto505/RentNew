689dc7e14d63bb7c7c7809c301b894ecbfa8a641
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2025-08-30 22:36:47 +0400

    fix ImageUploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < Shrine
+Float class ImageUploader < Shrine
   plugin :validation_helpers
   plugin :processing
   plugin :derivatives
@@ -12,7 +12,7 @@ class ImageUploader < Shrine
     context = options[:context] || {}
     record = context[:record]
     name = context[:name]
-    
+
     if record.is_a?(User) && name == :company_avatar
       # Avatar compression to ~100kb
       compress_image(io, max_size: 100 * 1024, quality: 75)
@@ -28,43 +28,38 @@ class ImageUploader < Shrine
 
   def compress_image(io, max_size:, quality:)
     require "image_processing/mini_magick"
-    
-    # Read the original size
+
     io.rewind
-    original_content = io.read
-    original_size = original_content.bytesize
-    
-    return io if original_size <= max_size
-    
-    # Reset io position
-    io.rewind
-    
-    # Start with the specified quality and try to compress
     current_quality = quality
-    
-    # Try different compression levels
-    5.times do
+    processed_io = nil
+
+    # Try different compression levels until we reach target size
+    8.times do
       begin
+        io.rewind
         processed_io = ImageProcessing::MiniMagick
           .source(io)
-          .resize_to_limit(2048, 2048)
+          .resize_to_limit(1920, 1920)
           .quality(current_quality)
+          .convert("jpg")
           .call
-        
+
+        Rails.logger.info "Compressed image: quality=#{current_quality}, size=#{processed_io.size} bytes, target=#{max_size} bytes"
+
         if processed_io.size <= max_size
           return processed_io
         end
-        
-        break if current_quality <= 30
+
+        break if current_quality <= 20
         current_quality -= 10
-        io.rewind
       rescue => e
         Rails.logger.error "Image compression error: #{e.message}"
+        io.rewind
         return io
       end
     end
-    
-    # If we couldn't compress enough, return the last attempt
+
+    # Return compressed version even if it's still larger than target
     processed_io || io
   end
 end
